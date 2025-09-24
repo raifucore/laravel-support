062a095606a1c97b9c5e029c5832d9e8190ac361
RaifuCore <raifuscripts@gmail.com>
2025-09-24 10:44:41 +0300

    add isCollapsed menu

--- src/Services/Layout/Menu.php
+++ src/Services/Layout/Menu.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Collection;
 
 class Menu
 {
+    protected bool $isCollapsed = false;
     protected string|null $currentModule = null;
     protected string|null $currentItem = null;
     protected Collection $items;
@@ -16,12 +17,48 @@ class Menu
         $this->items = collect();
     }
 
+    public function isCollapsed(): bool
+    {
+        return $this->isCollapsed;
+    }
+
+    public function getCurrentModule(): ?string
+    {
+        return $this->currentModule;
+    }
+
+    public function getCurrentItem(): ?string
+    {
+        return $this->currentItem;
+    }
+
     /**
      * @return Collection<MenuItemDto>
      */
     public function getItems(): Collection
     {
         return $this->items;
+    }
+
+    public function setIsCollapsed(bool $isCollapsed): self
+    {
+        $this->isCollapsed = $isCollapsed;
+
+        return $this;
+    }
+
+    public function setCurrentModule(?string $currentModule): self
+    {
+        $this->currentModule = $currentModule;
+
+        return $this;
+    }
+
+    public function setCurrentItem(?string $currentItem): self
+    {
+        $this->currentItem = $currentItem;
+
+        return $this;
     }
 
     public function addItem(MenuItemDto $item): self
