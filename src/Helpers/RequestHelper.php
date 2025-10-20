09153b7e9d8f1e080fb1d94ef832732b74b83a44
RaifuCore <raifuscripts@gmail.com>
2025-10-20 06:27:43 +0300

    add request helper

---
+++ src/Helpers/RequestHelper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace RaifuCore\Support\Helpers;
+
+use Illuminate\Http\Request;
+
+class RequestHelper
+{
+    public static function isBot(Request $request = null): bool
+    {
+        $userAgent = $request
+            ? $request->userAgent()
+            : request()->userAgent();
+
+        if (empty($userAgent)) {
+            return false;
+        }
+
+        $bots = [
+            // Yandex
+            'YandexBot', 'YandexAccessibilityBot', 'YandexMobileBot', 'YandexDirectDyn', 'YandexScreenshotBot',
+            'YandexImages', 'YandexVideo', 'YandexVideoParser', 'YandexMedia', 'YandexBlogs', 'YandexFavicons',
+            'YandexWebmaster', 'YandexPagechecker', 'YandexImageResizer', 'YandexAdNet', 'YandexDirect',
+            'YaDirectFetcher', 'YandexCalendar', 'YandexSitelinks', 'YandexMetrika', 'YandexNews',
+            'YandexNewslinks', 'YandexCatalog', 'YandexAntivirus', 'YandexMarket', 'YandexVertis',
+            'YandexForDomain', 'YandexSpravBot', 'YandexSearchShop', 'YandexMedianaBot', 'YandexOntoDB',
+            'YandexOntoDBAPI', 'YandexTurbo', 'YandexVerticals',
+
+            // Google
+            'Googlebot', 'Googlebot-Image', 'Mediapartners-Google', 'AdsBot-Google', 'APIs-Google',
+            'AdsBot-Google-Mobile', 'AdsBot-Google-Mobile', 'Googlebot-News', 'Googlebot-Video',
+            'AdsBot-Google-Mobile-Apps',
+
+            // Other
+            'Mail.RU_Bot', 'bingbot', 'Accoona', 'ia_archiver', 'Ask Jeeves', 'OmniExplorer_Bot', 'W3C_Validator',
+            'WebAlta', 'YahooFeedSeeker', 'Yahoo!', 'Ezooms', 'Tourlentabot', 'MJ12bot', 'AhrefsBot',
+            'SearchBot', 'SiteStatus', 'Nigma.ru', 'Baiduspider', 'Statsbot', 'SISTRIX', 'AcoonBot', 'findlinks',
+            'proximic', 'OpenindexSpider', 'statdom.ru', 'Exabot', 'Spider', 'SeznamBot', 'oBot', 'C-T bot',
+            'Updownerbot', 'Snoopy', 'heritrix', 'Yeti', 'DomainVader', 'DCPbot', 'PaperLiBot', 'StackRambler',
+            'msnbot', 'msnbot-media', 'msnbot-news',
+        ];
+
+        foreach ($bots as $bot) {
+            if (stripos($userAgent, $bot) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
