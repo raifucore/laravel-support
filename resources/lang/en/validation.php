02f3b3a6d383e04cd9db19f09c547b9e3a1d1f5f
RaifuCore <raifuscripts@gmail.com>
2025-11-01 08:48:54 +0300

    add telegram rule

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    'email' => 'The <b>:attribute</b> field must be a valid email address',
+    'email' => 'The :attribute field must be a valid email address',
     'arrayHash' => 'Invalid signature. Refresh the page and try again',
-    'positiveNumber' => 'The <b>:attribute</b> value must be greater than zero',
-    'notNegativeNumber' => 'The <b>:attribute</b> field must not be a negative number',
-    'subdomain' => 'The <b>:attribute</b> field must contain only English letters and digits. Max: 62 characters',
+    'positiveNumber' => 'The :attribute value must be greater than zero',
+    'notNegativeNumber' => 'The :attribute field must not be a negative number',
+    'subdomain' => 'The :attribute field must contain only English letters and digits. Max: 62 characters',
+    'telegram' => 'Поле :attribute must be valid username. From 5 to 32 symbols',
 ];
--- src/Rules/Telegram.php
+++ src/Rules/Telegram.php
@@ -5,12 +5,12 @@ namespace RaifuCore\Support\Rules;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
-class Subdomain implements ValidationRule
+class Telegram implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (!preg_match('^[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?$', $value)) {
-            $fail(__('raifucore::validation.subdomain'));
+        if (!preg_match('^[a-zA-Z][a-zA-Z0-9_]{4,31}$', $value)) {
+            $fail(__('raifucore::validation.telegram'));
         }
     }
 }
